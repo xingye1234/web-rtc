c813578f05621973a713720677ac3dd5d98a32b7
wangkexing <1989163733@qq.com>
2024-10-12 15:56:09 +0800

    feat:add video chat

--- project/src/components/WebRtc.tsx
+++ project/src/components/WebRtc.tsx
@@ -15,26 +15,55 @@ interface IMessage {
 }
 
 export default function WebRtc() {
+  const peerRef = useRef<any>(peer);
+  //对话链接
   const conn = useRef<any>();
+  // 视频连接
+  const currentCallRef = useRef<any>();
+
   const scrollRef = useRef<HTMLDivElement>(null);
   const [localId, setLocalId] = useState<string>("");
   const [remoteId, setRemoteId] = useState<string>("");
+
+  const localVideoRef = useRef<HTMLVideoElement>(null);
+  const remoteVideoRef = useRef<HTMLVideoElement>(null);
+
   const [messages, setMessages] = useState<string>("");
   const [messageList, setMessageList] = useState<IMessage[]>([]);
 
   const initPeer = () => {
-    peer!.on("open", (id) => {
+    peerRef.current!.on("open", (id:any) => {
       setLocalId(id);
     });
-    peer!.on("connection", (connection) => {
+    peerRef.current!.on("connection", (connection:any) => {
       connection.on("data", (data: any) => {
         setMessageList((prev) => [...prev, { type: MessageType.REMOTE, data }]);
       });
     });
+
+    peerRef.current!.on("call", async (call: any) => {
+      if (window.confirm(`是否接受 ${call.peer}?`)) {
+        const stream = await navigator.mediaDevices.getUserMedia({
+          video: true,
+          audio: true,
+        });
+        localVideoRef.current!.srcObject = stream;
+        localVideoRef.current!.play();
+
+        call.answer(stream);
+
+        currentCallRef.current = call;
+
+        call.on("stream", (stream: any) => {
+          remoteVideoRef.current!.srcObject = stream;
+          remoteVideoRef.current!.play();
+        });
+      }
+    });
   };
 
   const connectPeer = () => {
-    conn.current = peer!.connect(remoteId);
+    conn.current = peerRef.current!.connect(remoteId);
     conn.current.on("open", () => {
       message.success("连接成功");
     });
@@ -61,6 +90,45 @@ export default function WebRtc() {
     conn.current.send(messages);
 
     setMessages("");
+  };
+
+  const endCall = () => {
+    if (currentCallRef.current) {
+      currentCallRef.current.close();
+    }
+  };
+
+  //视频通话
+  const handleCallVideo = async () => {
+    if (!remoteId) {
+      message.error("请输入远程ID");
+      return;
+    }
+
+    const stream = await navigator.mediaDevices.getUserMedia({
+      video: true,
+      audio: true,
+    });
+    localVideoRef.current!.srcObject = stream;
+    localVideoRef.current!.play();
+    handleConnectPeer();
+
+    const call = peerRef.current!.call(remoteId, stream);
+    call.on("stream", (stream: any) => {
+      remoteVideoRef.current!.srcObject = stream;
+      remoteVideoRef.current!.play();
+    });
+    call.on("error", (err: any) => {
+      console.error(err);
+    });
+    call.on("close", () => {
+      endCall();
+    });
+
+    currentCallRef.current!.on("stream", (stream: any) => {
+      remoteVideoRef.current!.srcObject = stream;
+      remoteVideoRef.current!.play();
+    });
   };
 
   useEffect(() => {
@@ -101,6 +169,31 @@ export default function WebRtc() {
         <Button type="primary" onClick={handleConnectPeer}>
           连接
         </Button>
+      </div>
+      <div className="flex mt-4 flex-col">
+        <div className="flex gap-x-2">
+          <Button type="primary" onClick={handleCallVideo}>
+            视频通话
+          </Button>
+          <Button variant="solid" color="danger" onClick={endCall}>
+            结束通话
+          </Button>
+        </div>
+
+        <div className="w-full flex gap-x-2 mt-4">
+          <video
+            className="w-1/2 h-48"
+            ref={localVideoRef}
+            autoPlay
+            controls
+          ></video>
+          <video
+            className="w-1/2 h-48"
+            ref={remoteVideoRef}
+            autoPlay
+            controls
+          ></video>
+        </div>
       </div>
       <div className="mt-4">
         <ScrollArea
